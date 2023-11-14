15f9b177b8228e79895dae8c1b84fd91d684ae4a
lll11lll <nicholasgreiner1@gmail.com>
2023-11-14 18:15:23 -0500

    added comments

--- player.py
+++ player.py
@@ -3,6 +3,7 @@ from math import sqrt
 
 class Player(pg.sprite.Sprite): 
     def __init__(self, x, y):
+        # Creation of player, and creating the rect with provide x and y
         self.survivor = pg.image.load('Survivor1/survivor1Hold.png')
         self.rect = self.survivor.get_rect()
         self.rect.x = x
@@ -11,26 +12,48 @@ class Player(pg.sprite.Sprite):
 
 
     def draw(self, window):
+        # Drawing the sprite to the window
         window.blit(self.survivor, self.rect)
     
     def update(self):
+        # Checking for key presses and moving the player accordingly
+
         key = pg.key.get_pressed()
+
+        # Checks if the player uses 2 keys at once and adjusts their speed to the same value as if they were only using 1 key
+        # Up and Right
         if key[pg.K_w] and key[pg.K_d]:
-            self.rect.x += (sqrt(12.5) + 1)
-            self.rect.y -= (sqrt(12.5) + 1)
-            print(self.rect.y)
-            print(self.rect.x)
-            
+            self.rect.x += (sqrt(12.5))
+            self.rect.y -= (sqrt(12.5))
+        
+        # Up and Left
+        elif key[pg.K_w] and key[pg.K_a]:
+            self.rect.x -= (sqrt(12.5))
+            self.rect.y -= (sqrt(12.5))
+        
+        # Down and Right
+        elif key[pg.K_s] and key[pg.K_d]:
+            self.rect.x += (sqrt(12.5))
+            self.rect.y += (sqrt(12.5))
+        # Down and Left
+        elif key[pg.K_s] and key[pg.K_a]:
+            self.rect.x -= (sqrt(12.5))
+            self.rect.y += (sqrt(12.5))
+        
+        # Up
         elif key[pg.K_w]:
             self.rect.y -= self.speed
-            print(self.rect.y)
-            print(self.rect.x)
+
+        # Down
         elif key[pg.K_a]:
             self.rect.x -= self.speed
-
-        elif key[pg.K_s]:
-            self.rect.y += self.speed
+        
+        # Right
         elif key[pg.K_d]:
             self.rect.x += self.speed
+        
+        # Left
+        elif key[pg.K_s]:
+            self.rect.y += self.speed
         
             
