44124a261fc690d8204161e80153b706803e5553
ponpon14 <nsuzuki1@ufl.edu>
2023-11-14 17:05:05 -0500

    added background wokring with main

--- main.py
+++ main.py
@@ -10,3 +10,16 @@ WIN = pg.display.set_mode((WIDTH, HEIGHT))
 
 backgroundImage =  pg.transform.scale(pg.image.load('background.png'), (WIDTH, HEIGHT))
 
+def drawWindow():
+    WIN.blit(backgroundImage, (0,0))
+    pg.display.update()
+
+def main():
+    run = True
+    while run:
+        clock.tick(FPS)
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+        drawWindow()
+main()
